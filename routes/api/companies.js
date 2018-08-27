7bf1a68abc25602a8ac91b2865e41456e36b97af
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-08-26 21:49:50 -0300

    adicionando funcionalidades de busca

--- routes/api/companies.js
+++ routes/api/companies.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
+const isEmpty = require("../../validation/is-empty");
 
 const router = express.Router();
 
@@ -10,21 +11,41 @@ const Companie = require("../../models/Companie");
 // @route GET api/companies/
 // @desc get companies
 // @access Public
-router.get(
-  "/",
+router.get("/", (req, res) => {
+  // eval(require("locus"));
+  filter = {};
+  req.query.name
+    ? (filter.name = new RegExp(escapeRegex(req.query.name), "gi"))
+    : "";
+  req.query.adress
+    ? (filter.adress = new RegExp(escapeRegex(req.query.adress), "gi"))
+    : "";
+  req.query.phone
+    ? (filter.phone = new RegExp(escapeRegex(req.query.phone), "gi"))
+    : "";
+  req.query.cnpj
+    ? (filter.cnpj = new RegExp(escapeRegex(req.query.cnpj), "gi"))
+    : "";
 
-  (req, res) => {
-    Companie.find().then(comp => {
-      res.json(comp);
-    });
-  }
-);
+  Companie.find(filter, (err, doc) => {
+    if (err) return res.status(400).json(err);
+
+    if (doc.length < 1) {
+      return res.status(400).json({ find: "Não encontrado!" });
+    }
+    res.json(doc);
+  });
+});
+
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
 // @route GET api/companies/:id
 // @desc get companie by id
 // @access Public
 router.get("/:id", (req, res) => {
-  const id = req.params.id;
+  const id = req.body.id;
   Companie.findById(id, (err, comp) => {
     if (err)
       return res.status(400).json({ id: "Não encontrado empresa com essa id" });
@@ -44,7 +65,8 @@ router.post("/", (req, res) => {
       const newComp = new Companie({
         name: req.body.name,
         adress: req.body.adress,
-        phone: req.body.phone
+        phone: req.body.phone,
+        cnpj: req.body.cnpj
       });
 
       newComp
